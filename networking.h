ecff010c75016cb7df84ecf70aca1308072285bb
Timmy Ngo <timmyn@nycstudents.net>
2026-01-07 11:31:29 -0500

    tim edoted file heder

---
+++ networking.h
@@ -0,0 +1,11 @@
+#ifndef NETWORKING_H
+#define NETWORKING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
