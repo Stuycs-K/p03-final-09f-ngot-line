e5681b65288548d3c71d531c132787943ede2a89
Ethan Lin <ethanl157@nycstudents.net>
2026-01-07 13:49:08 -0600

    filling in networking.c

--- DEVLOG-Ethan.md
+++ DEVLOG-Ethan.md
@@ -7,5 +7,5 @@ This document must be updated daily every time you finish a work session.
 ### 2026-01-06 - Basic stuff
 added server.c, client.c, networking.c, networking.h. Starting work on server.c.
 
-### 2024-01-03 - Brief description
-Expanded description including how much time was spent on task.
+### 2026-01-07 - networking
+working on networking.h. reminder: log client ip for each client in chat
--- networking.c
+++ networking.c
@@ -1,8 +1,51 @@
 #include "networking.h"
 
 
-int server_setup() {}
+int server_setup() {
+  struct addrinfo * hints, * results;
+  hints = calloc(1,sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+  hints->ai_flags = AI_PASSIVE;
+  getaddrinfo(NULL, PORT , hints, &results);
 
-int server_tcp_handshake(int listen_socket){}
+  int clientd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
 
-int client_tcp_handshake(char * server_address) {}
+  int yes = 1;
+  int sockOpt =  setsockopt(clientd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  err(sockOpt,"sockopt  error");
+
+  bind( clientd, results->ai_addr, results->ai_addrlen);
+
+  free(hints);
+  freeaddrinfo(results);
+
+  return clientd;
+}
+
+int server_tcp_handshake(int listen_socket){
+  int client_socket;
+
+
+  return client_socket;
+}
+
+int client_tcp_handshake(char * server_address) {
+  struct addrinfo * hints, * results;
+  hints = calloc(1,sizeof(struct addrinfo));
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+
+  int serverd = getaddrinfo(NULL, PORT , &hints, &results);
+
+  free(hints);
+  freeaddrinfo(results);
+  return serverd;
+}
+
+void err(int i, char*message){
+  if(i < 0){
+	  printf("Error: %s - %s\n",message, strerror(errno));
+  	exit(1);
+  }
+}
