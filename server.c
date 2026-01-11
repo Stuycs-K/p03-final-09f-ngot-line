a5f300f68507c2d0c32c7d292f702eefef0c74de
Timmy <timmyn@nycstudents.net>
2026-01-11 16:11:56 -0500

    server.c handshake implementation

--- server.c
+++ server.c
@@ -1,4 +1,6 @@
 #include "networking.h"
+
+//handle zomb-processes
 void sighandler(int sig) {
     if (sig == SIGCHLD) {
         while (waitpid(-1, NULL, WNOHANG) > 0);
@@ -9,40 +11,58 @@ void sighandler(int sig) {
 int main() {
     signal(SIGCHLD, sighandler); //trying to fix z-processes issue
 
-    int listen_socket, client_socket;
-    struct sockaddr_in server_addr;
-    struct message incoming;
+    // setups Shared Memory for the socket table
+    // This allows children to see everyone's socket ID for broadcasting
+    int shmid = shmget(SHM_KEY, sizeof(int) * MAX_CLIENTS, IPC_CREAT | 0644);
+    int *client_sockets = shmat(shmid, NULL, 0);
 
-    listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // initalizing all slots to -1 (or empty)
+    for(int i=0; i<MAX_CLIENTS; i++) client_sockets[i] = -1;
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    int listen_socket = server_setup();
+    printf("[server] Multi-user server started on port %s...\n", PORT);
 
-    bind(listen_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    listen(listen_socket, 5);
-    printf("[server] Multi-user server started on port %d...\n", PORT);
-
+    //implements our handshake logic to connect an disconnetc users
     while (1) {
-        client_socket = accept(listen_socket, NULL, NULL);
+        int client_socket = server_tcp_handshake(listen_socket);
 
-        // Fork a new process for each client
+        //finds an empty spot in shared memory for this new user
+        int slot = -1;
+        for(int i=0; i<MAX_CLIENTS; i++) {
+            if (client_sockets[i] == -1) {
+                client_sockets[i] = client_socket;
+                slot = i;
+                break;
+            }
+        }
+
         int f = fork();
         if (f == 0) { // CHILD PROCESS
-            close(listen_socket); // Child doesn't need the listener
+            close(listen_socket);
+            struct message incoming;
 
+            // Read from THIS client
             while (read(client_socket, &incoming, sizeof(struct message)) > 0) {
                 printf("[%s]: %s", incoming.username, incoming.text);
-                // Later, we will add code here to broadcast to other users
+
+                // broadcast to everyone else in the table
+                for (int i = 0; i < MAX_CLIENTS; i++) {
+                    int target_fd = client_sockets[i];
+                    if (target_fd != -1 && target_fd != client_socket) {
+                        write(target_fd, &incoming, sizeof(struct message));
+                    }
+                }
             }
 
-            printf("[server] Client disconnected.\n");
+            // Cleanup when they leave
+            printf("[server] Client %s disconnected.\n", incoming.username);
+            client_sockets[slot] = -1; // Vacate the spot
             close(client_socket);
-            exit(0); // Kill child process when client leaves
-        } else { // PARENT PROCESS
-            close(client_socket); // Parent doesn't need this specific client's socket
+            exit(0);
+        }
+        else { // PARENT PROCESS
+            close(client_socket);
         }
     }
     return 0;
 }
-
